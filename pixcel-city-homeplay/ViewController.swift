f0605c28ef69fc70b99a56d80c46c66499e5c7b0
Vansa Pha <vs.lov.rs@gmail.com>
2017-10-11 08:33:01 +0700

    Initial Commit

---
+++ pixcel-city-homeplay/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pixcel-city-homeplay
+//
+//  Created by Vansa Pha on 10/11/17.
+//  Copyright © 2017 Vansa Pha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
